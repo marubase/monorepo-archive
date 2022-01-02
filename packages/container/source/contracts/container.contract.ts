ea0ffd0d7d21ce341d0b127127f58604d8d6de3f
Endy Jasmi <endyjasmi@gmail.com>
2022-01-02 19:19:30 +0800

    feat(@marubase/container): add container contract

---
+++ packages/container/source/contracts/container.contract.ts
@@ -0,0 +1,65 @@
+import {
+  Bindable,
+  Binding,
+  RegistryContract,
+  Resolvable,
+  ResolvableKey,
+  ResolvableTag,
+} from "./registry.contract.js";
+import { ResolverDependencies } from "./resolver.contract.js";
+import { ScopeContract } from "./scope.contract.js";
+
+export interface ContainerContract {
+  new (registry: RegistryContract, scope: ScopeContract): this;
+
+  bind(bindable: Bindable): Binding;
+
+  bound(bindable: Bindable): boolean;
+
+  fork(): this;
+
+  getRegistry(): RegistryContract;
+
+  getScope(): ScopeContract;
+
+  resolve<Result>(resolvable: Resolvable, ...args: Array<unknown>): Result;
+
+  resolveAlias<Result>(alias: ResolvableKey, ...args: Array<unknown>): Result;
+
+  resolveClass<Result>(
+    target: Function,
+    dependencies: ResolverDependencies,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveConstant<Result>(constant: unknown): Result;
+
+  resolveConstructor<Result>(
+    target: Function,
+    dependencies: ResolverDependencies,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveFactory<Result>(
+    resolvable: Resolvable,
+    ...args: Array<unknown>
+  ): ResolveFactory<Result>;
+
+  resolveFunction<Result>(
+    target: Function,
+    dependencies: ResolverDependencies,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveMethod<Result>(
+    target: Function | Object,
+    method: string | symbol,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveTag<Result>(tag: ResolvableTag, ...args: Array<unknown>): Result;
+
+  unbind(bindable: Bindable): this;
+}
+
+export type ResolveFactory<Result> = (...args: Array<unknown>) => Result;
--- packages/container/source/contracts/index.ts
+++ packages/container/source/contracts/index.ts
@@ -1,3 +1,4 @@
+export * from "./container.contract.js";
 export * from "./registry.contract.js";
 export * from "./resolver.contract.js";
 export * from "./scope.contract.js";
