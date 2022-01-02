d03eddcb1dc458fd906324bb2a522efda9276ae9
Endy Jasmi <endyjasmi@gmail.com>
2022-01-02 15:32:39 +0800

    feat(@marubase/container): update scope contract

--- packages/container/source/contracts/scope.contract.ts
+++ packages/container/source/contracts/scope.contract.ts
@@ -1,9 +1,14 @@
 export interface ScopeContract {
+  new (): this;
+  new (parent: ScopeContract): this;
+
   clear(): this;
 
   fork(): this;
 
   get(key: ScopeKey): unknown;
+
+  getParent(): this | undefined;
 
   has(key: ScopeKey): boolean;
 
