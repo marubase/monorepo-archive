ca2a36fd75f2f3fea57430b6dd5e2b28177e3466
Endy Jasmi <endyjasmi@gmail.com>
2022-01-04 18:27:22 +0800

    refactor(@marubase/container): remove context error implementation

--- packages/container/source/errors/context-error.ts
+++
@@ -1,3 +0,0 @@
-import { BaseError } from "./base-error.js";
-
-export class ContextError extends BaseError {}
--- packages/container/source/errors/index.ts
+++ packages/container/source/errors/index.ts
@@ -1,3 +1,2 @@
 export * from "./base-error.js";
-export * from "./context-error.js";
 export * from "./scope-error.js";
