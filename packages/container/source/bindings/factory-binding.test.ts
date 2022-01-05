a29841dbf0d0d80e8e78db616bc8ae0026755bcd
Endy Jasmi <endyjasmi@gmail.com>
2022-01-05 21:10:12 +0800

    test(@marubase/container): add factory binding test

---
+++ packages/container/source/bindings/factory-binding.test.ts
@@ -0,0 +1,37 @@
+import { expect } from "chai";
+import { anything, instance, mock, reset, when } from "ts-mockito";
+import { CacheContract } from "../contracts/cache.contract.js";
+import {
+  ResolveFactory,
+  ResolverContract,
+} from "../contracts/resolver.contract.js";
+import { FactoryBinding } from "./factory-binding.js";
+
+suite("FactoryBinding", function () {
+  let binding: FactoryBinding;
+  let mockCache: CacheContract;
+  let mockResolver: ResolverContract;
+  setup(async function () {
+    mockCache = mock<CacheContract>();
+    mockResolver = mock<ResolverContract>();
+    binding = new FactoryBinding(instance(mockResolver), "test");
+  });
+  teardown(async function () {
+    reset(mockCache);
+    reset(mockResolver);
+  });
+
+  suite("#resolve(cache)", function () {
+    test("should return factory", async function () {
+      when(mockResolver.resolve<Date>(anything(), "test")).thenReturn(
+        new Date(),
+      );
+
+      const factory = binding.resolve<ResolveFactory<Date>>(
+        instance(mockCache),
+      );
+      const actualResult = factory();
+      expect(actualResult).to.be.an.instanceOf(Date);
+    });
+  });
+});
