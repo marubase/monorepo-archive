10d128a58cd24c90902af6d8e3abdcb3c76b542c
Endy Jasmi <endyjasmi@gmail.com>
2022-01-05 12:18:25 +0800

    refactor(@marubase/container): remove context contract

--- packages/container/source/contracts/index.ts
+++ packages/container/source/contracts/index.ts
@@ -1,6 +1,5 @@
 export * from "./cache.contract.js";
 export * from "./container.contract.js";
-export * from "./context.contract.js";
 export * from "./plugin.contract.js";
 export * from "./registry.contract.js";
 export * from "./resolver.contract.js";
