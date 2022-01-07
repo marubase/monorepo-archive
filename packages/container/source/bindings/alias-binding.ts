251a7bff348029d722fb362732ac0bc1370a04bb
Endy Jasmi <endyjasmi@gmail.com>
2022-01-07 21:14:19 +0800

    feat(@marubase/container): update alias binding implementation

--- packages/container/source/bindings/alias-binding.ts
+++ packages/container/source/bindings/alias-binding.ts
@@ -1,6 +1,7 @@
 import { BindingContract, BindingKey } from "../contracts/binding.contract.js";
 import { CacheContract } from "../contracts/cache.contract.js";
 import { ResolverContract } from "../contracts/resolver.contract.js";
+import { BindingError } from "../errors/binding.error.js";
 import { BaseBinding } from "./base-binding.js";
 
 export class AliasBinding extends BaseBinding implements BindingContract {
@@ -11,10 +12,14 @@ export class AliasBinding extends BaseBinding implements BindingContract {
   public resolve<Result>(
     cache: CacheContract,
     ...args: Array<unknown>
-  ): Result | undefined {
+  ): Result {
     const binding = this._resolver.findByKey(this.alias);
-    return typeof binding !== "undefined"
-      ? (binding.resolve(cache, ...args) as Result)
-      : undefined;
+    if (typeof binding === "undefined") {
+      const context = `Resolving binding.`;
+      const problem = `Binding key not found.`;
+      const solution = `Please use another binding key as alias.`;
+      throw new BindingError(`${context} ${problem} ${solution}`);
+    }
+    return binding.resolve(cache, ...args) as Result;
   }
 }
