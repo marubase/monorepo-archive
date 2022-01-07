1a3aa6e9071392b649dd8924bdbc0ba551d8228d
Endy Jasmi <endyjasmi@gmail.com>
2022-01-07 21:44:01 +0800

    feat(@marubase/container): add resolver implementation

--- packages/container/source/index.ts
+++ packages/container/source/index.ts
@@ -2,3 +2,4 @@ export * as bindings from "./bindings/index.js";
 export * from "./cache.js";
 export * as contracts from "./contracts/index.js";
 export * as errors from "./errors/index.js";
+export * from "./resolver.js";
---
+++ packages/container/source/resolver.ts
@@ -0,0 +1,220 @@
+import { ConstantBinding } from "./bindings/constant-binding.js";
+import { ConstructorBinding } from "./bindings/constructor-binding.js";
+import { FunctionBinding } from "./bindings/function-binding.js";
+import { KeyBinding } from "./bindings/key-binding.js";
+import { MethodBinding } from "./bindings/method-binding.js";
+import { TagBinding } from "./bindings/tag-binding.js";
+import {
+  BindingContract,
+  BindingKey,
+  BindingTag,
+} from "./contracts/binding.contract.js";
+import { CacheContract } from "./contracts/cache.contract.js";
+import {
+  Bindable,
+  Binding,
+  BindingFactory,
+  Resolvable,
+  ResolverContract,
+} from "./contracts/resolver.contract.js";
+
+export class Resolver implements ResolverContract {
+  protected _bindingFactory: BindingFactory;
+
+  protected _keyIndex: Map<BindingKey, BindingContract> = new Map();
+
+  protected _tagIndex: Map<BindingKey, Set<BindingContract>> = new Map();
+
+  public constructor(bindingFactory: BindingFactory) {
+    this._bindingFactory = bindingFactory;
+  }
+
+  public bind(bindable: Bindable): Binding {
+    const bindingKey =
+      typeof bindable === "function" ? bindable.name : bindable;
+    return {
+      toAlias: (alias) => this.createAliasBinding(alias).setKey(bindingKey),
+      toClass: (target) => this.createClassBinding(target).setKey(bindingKey),
+      toConstant: (constant) =>
+        this.createConstantBinding(constant).setKey(bindingKey),
+      toConstructor: (target) =>
+        this.createConstructorBinding(target).setKey(bindingKey),
+      toFunction: (target) =>
+        this.createFunctionBinding(target).setKey(bindingKey),
+      toKey: (key) => this.createKeyBinding(key).setKey(bindingKey),
+      toMethod: (target, method) =>
+        this.createMethodBinding(target, method).setKey(bindingKey),
+      toTag: (tag) => this.createTagBinding(tag).setKey(bindingKey),
+    };
+  }
+
+  public createAliasBinding(alias: BindingKey): BindingContract {
+    return this.createKeyBinding(alias);
+  }
+
+  public createClassBinding(target: Function): BindingContract {
+    return this.createConstructorBinding(target);
+  }
+
+  public createConstantBinding(constant: unknown): BindingContract {
+    return this._bindingFactory.createConstantBinding(this, constant);
+  }
+
+  public createConstructorBinding(target: Function): BindingContract {
+    return this._bindingFactory.createConstructorBinding(this, target);
+  }
+
+  public createFunctionBinding(target: Function): BindingContract {
+    return this._bindingFactory.createFunctionBinding(this, target);
+  }
+
+  public createKeyBinding(key: BindingKey): BindingContract {
+    return this._bindingFactory.createKeyBinding(this, key);
+  }
+
+  public createMethodBinding(
+    target: Object | Function,
+    method: string | symbol,
+  ): BindingContract {
+    return this._bindingFactory.createMethodBinding(this, target, method);
+  }
+
+  public createTagBinding(tag: BindingTag): BindingContract {
+    return this._bindingFactory.createTagBinding(this, tag);
+  }
+
+  public deindexByKey(binding: BindingContract, key: BindingKey): this {
+    this._keyIndex.delete(key);
+    return this;
+  }
+
+  public deindexByTag(binding: BindingContract, tag: BindingTag): this {
+    if (!this._tagIndex.has(tag)) this._tagIndex.set(tag, new Set());
+    const tags = this._tagIndex.get(tag) as Set<BindingContract>;
+    tags.delete(binding);
+    return this;
+  }
+
+  public findByKey(key: BindingKey): BindingContract | undefined {
+    return this._keyIndex.get(key);
+  }
+
+  public findByTag(tag: BindingTag): BindingContract[] {
+    if (!this._tagIndex.has(tag)) this._tagIndex.set(tag, new Set());
+    const tags = this._tagIndex.get(tag) as Set<BindingContract>;
+    return Array.from(tags);
+  }
+
+  public indexByKey(binding: BindingContract, key: BindingKey): this {
+    this._keyIndex.set(key, binding);
+    return this;
+  }
+
+  public indexByTag(binding: BindingContract, tag: BindingTag): this {
+    if (!this._tagIndex.has(tag)) this._tagIndex.set(tag, new Set());
+    const tags = this._tagIndex.get(tag) as Set<BindingContract>;
+    tags.add(binding);
+    return this;
+  }
+
+  public resolve<Result>(
+    cache: CacheContract,
+    resolvable: Resolvable,
+    ...args: Array<unknown>
+  ): Result {
+    const bindingKey = Array.isArray(resolvable)
+      ? typeof resolvable[0] === "function"
+        ? resolvable[0].name
+        : resolvable[0]
+      : typeof resolvable === "function"
+      ? resolvable.name
+      : resolvable;
+    return this.resolveKey(cache, bindingKey, ...args);
+  }
+
+  public resolveAlias<Result>(
+    cache: CacheContract,
+    alias: BindingKey,
+    ...args: unknown[]
+  ): Result {
+    return this.createAliasBinding(alias).resolve(cache, ...args);
+  }
+
+  public resolveClass<Result>(
+    cache: CacheContract,
+    target: Function,
+    ...args: unknown[]
+  ): Result {
+    return this.createClassBinding(target).resolve(cache, ...args);
+  }
+
+  public resolveConstant<Result>(
+    cache: CacheContract,
+    constant: unknown,
+    ...args: unknown[]
+  ): Result {
+    return this.createConstantBinding(constant).resolve(cache, ...args);
+  }
+
+  public resolveConstructor<Result>(
+    cache: CacheContract,
+    target: Function,
+    ...args: unknown[]
+  ): Result {
+    return this.createConstructorBinding(target).resolve(cache, ...args);
+  }
+
+  public resolveFunction<Result>(
+    cache: CacheContract,
+    target: Function,
+    ...args: unknown[]
+  ): Result {
+    return this.createFunctionBinding(target).resolve(cache, ...args);
+  }
+
+  public resolveKey<Result>(
+    cache: CacheContract,
+    key: BindingKey,
+    ...args: unknown[]
+  ): Result {
+    return this.createKeyBinding(key).resolve(cache, ...args);
+  }
+
+  public resolveMethod<Result>(
+    cache: CacheContract,
+    target: Object | Function,
+    method: string | symbol,
+    ...args: unknown[]
+  ): Result {
+    return this.createMethodBinding(target, method).resolve(cache, ...args);
+  }
+
+  public resolveTag<Result>(
+    cache: CacheContract,
+    tag: BindingTag,
+    ...args: unknown[]
+  ): Result {
+    return this.createTagBinding(tag).resolve(cache, ...args);
+  }
+}
+
+export const DefaultBindingFactory: BindingFactory = {
+  createConstantBinding(resolver, constant) {
+    return new ConstantBinding(resolver, constant);
+  },
+  createConstructorBinding(resolver, target) {
+    return new ConstructorBinding(resolver, target);
+  },
+  createFunctionBinding(resolver, target) {
+    return new FunctionBinding(resolver, target);
+  },
+  createKeyBinding(resolver, key) {
+    return new KeyBinding(resolver, key);
+  },
+  createMethodBinding(resolver, target, method) {
+    return new MethodBinding(resolver, target, method);
+  },
+  createTagBinding(resolver, tag) {
+    return new TagBinding(resolver, tag);
+  },
+};
