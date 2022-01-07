7c76a924531195554f2b898be365fa03787e298e
Endy Jasmi <endyjasmi@gmail.com>
2022-01-07 20:51:40 +0800

    feat(@marubase/container): add method binding implementation

--- packages/container/source/bindings/index.ts
+++ packages/container/source/bindings/index.ts
@@ -3,4 +3,5 @@ export * from "./base-binding.js";
 export * from "./constant-binding.js";
 export * from "./constructor-binding.js";
 export * from "./function-binding.js";
+export * from "./method-binding.js";
 export * from "./tag-binding.js";
