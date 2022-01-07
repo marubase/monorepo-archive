20f38851d147edacea07093e1b4757a35458edbc
Endy Jasmi <endyjasmi@gmail.com>
2022-01-07 20:58:49 +0800

    feat(@marubase/container): update resolver contract

--- packages/container/source/contracts/resolver.contract.ts
+++ packages/container/source/contracts/resolver.contract.ts
@@ -7,6 +7,8 @@ import {
 import { CacheContract } from "./cache.contract.js";
 
 export interface ResolverContract {
+  bind(bindable: Bindable): Binding;
+
   createAliasBinding(alias: BindingKey): BindingContract;
 
   createClassBinding(target: Function): BindingContract;
@@ -45,7 +47,68 @@ export interface ResolverContract {
     resolvable: Resolvable,
     ...args: Array<unknown>
   ): Result;
+
+  resolveAlias<Result>(
+    cache: CacheContract,
+    alias: BindingKey,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveClass<Result>(
+    cache: CacheContract,
+    target: Function,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveConstant<Result>(
+    cache: CacheContract,
+    constant: unknown,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveConstructor<Result>(
+    cache: CacheContract,
+    target: Function,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveFunction<Result>(
+    cache: CacheContract,
+    target: Function,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveMethod<Result>(
+    cache: CacheContract,
+    target: Function | Object,
+    method: string | symbol,
+    ...args: Array<unknown>
+  ): Result;
+
+  resolveTag<Result>(
+    cache: CacheContract,
+    tag: BindingTag,
+    ...args: Array<unknown>
+  ): Result;
 }
+
+export type Bindable = Function | string | symbol;
+
+export type Binding = {
+  toAlias(alias: BindingKey): BindingContract;
+
+  toClass(target: Function): BindingContract;
+
+  toConstant(constant: unknown): BindingContract;
+
+  toConstructorBinding(target: Function): BindingContract;
+
+  toFunction(target: Function): BindingContract;
+
+  toMethod(target: Function | Object, method: string | symbol): BindingContract;
+
+  toTag(tag: BindingTag): BindingContract;
+};
 
 export type BindingFactory = {
   createAliasBinding(
