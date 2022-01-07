7b29ebb53f04b52ea6016df6e9f6f8ea3b9f4cd5
Endy Jasmi <endyjasmi@gmail.com>
2022-01-07 21:53:34 +0800

    feat(@marubase/container): add container implementation

---
+++ packages/container/source/container.ts
@@ -0,0 +1,126 @@
+import { Cache } from "./cache.js";
+import { BindingKey, BindingTag } from "./contracts/binding.contract.js";
+import { CacheContract } from "./contracts/cache.contract.js";
+import { ContainerContract } from "./contracts/container.contract.js";
+import {
+  Bindable,
+  Binding,
+  Resolvable,
+  ResolverContract,
+} from "./contracts/resolver.contract.js";
+import { DefaultBindingFactory, Resolver } from "./resolver.js";
+
+export class Container implements ContainerContract {
+  protected _cache: CacheContract;
+
+  protected _resolver: ResolverContract;
+
+  public constructor(cache?: CacheContract, resolver?: ResolverContract) {
+    this._cache = cache || new Cache();
+    this._resolver = resolver || new Resolver(DefaultBindingFactory);
+  }
+
+  public get cache(): CacheContract {
+    return this._cache;
+  }
+
+  public get resolver(): ResolverContract {
+    return this._resolver;
+  }
+
+  public bind(bindable: Bindable): Binding {
+    return this._resolver.bind(bindable);
+  }
+
+  public fork(): this {
+    const Static = this.constructor as typeof Container;
+    return new Static(this._cache.fork("container"), this._resolver) as this;
+  }
+
+  public resolve<Result>(
+    resolvable: Resolvable,
+    ...args: Array<unknown>
+  ): Result {
+    return this._resolver.resolve(
+      this._cache.fork("request"),
+      resolvable,
+      ...args,
+    );
+  }
+
+  public resolveAlias<Result>(
+    alias: BindingKey,
+    ...args: Array<unknown>
+  ): Result {
+    return this._resolver.resolveAlias(
+      this._cache.fork("request"),
+      alias,
+      ...args,
+    );
+  }
+
+  public resolveClass<Result>(
+    target: Function,
+    ...args: Array<unknown>
+  ): Result {
+    return this._resolver.resolveClass(
+      this._cache.fork("request"),
+      target,
+      ...args,
+    );
+  }
+
+  public resolveConstant<Result>(
+    constant: unknown,
+    ...args: Array<unknown>
+  ): Result {
+    return this._resolver.resolveConstant(
+      this._cache.fork("request"),
+      constant,
+      ...args,
+    );
+  }
+
+  public resolveConstructor<Result>(
+    target: Function,
+    ...args: Array<unknown>
+  ): Result {
+    return this._resolver.resolveConstructor(
+      this._cache.fork("request"),
+      target,
+      ...args,
+    );
+  }
+
+  public resolveFunction<Result>(
+    target: Function,
+    ...args: Array<unknown>
+  ): Result {
+    return this._resolver.resolveFunction(
+      this._cache.fork("request"),
+      target,
+      ...args,
+    );
+  }
+
+  public resolveKey<Result>(key: BindingKey, ...args: Array<unknown>): Result {
+    return this._resolver.resolveKey(this._cache.fork("request"), key, ...args);
+  }
+
+  public resolveMethod<Result>(
+    target: Object | Function,
+    method: string | symbol,
+    ...args: Array<unknown>
+  ): Result {
+    return this._resolver.resolveMethod(
+      this._cache.fork("request"),
+      target,
+      method,
+      ...args,
+    );
+  }
+
+  public resolveTag<Result>(tag: BindingTag, ...args: Array<unknown>): Result {
+    return this._resolver.resolveTag(this._cache.fork("request"), tag, ...args);
+  }
+}
--- packages/container/source/index.ts
+++ packages/container/source/index.ts
@@ -1,5 +1,6 @@
 export * as bindings from "./bindings/index.js";
 export * from "./cache.js";
+export * from "./container.js";
 export * as contracts from "./contracts/index.js";
 export * as errors from "./errors/index.js";
 export * from "./resolver.js";
