766df13b4d7ed8e1a4c7c31b5bb5c356c754258e
Endy Jasmi <endyjasmi@gmail.com>
2022-01-07 22:39:10 +0800

    feat(@marubase/container): update key binding implementation

--- packages/container/source/bindings/key-binding.ts
+++ packages/container/source/bindings/key-binding.ts
@@ -1,7 +1,6 @@
 import { BindingContract, BindingKey } from "../contracts/binding.contract.js";
 import { CacheContract } from "../contracts/cache.contract.js";
 import { ResolverContract } from "../contracts/resolver.contract.js";
-import { BindingError } from "../errors/binding.error.js";
 import { BaseBinding } from "./base-binding.js";
 
 export class KeyBinding extends BaseBinding implements BindingContract {
@@ -11,12 +10,6 @@ export class KeyBinding extends BaseBinding implements BindingContract {
 
   public resolve<Result>(cache: CacheContract, ...args: unknown[]): Result {
     const binding = this._resolver.findByKey(this.alias);
-    if (typeof binding === "undefined") {
-      const context = `Resolving binding.`;
-      const problem = `Binding key not found.`;
-      const solution = `Please use another binding key.`;
-      throw new BindingError(`${context} ${problem} ${solution}`);
-    }
     return binding.resolve(cache, ...args) as Result;
   }
 }
