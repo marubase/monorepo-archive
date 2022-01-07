cd300c32ad5fbafb089c7f874e7fbbcc9a8cc57a
Endy Jasmi <endyjasmi@gmail.com>
2022-01-07 20:49:03 +0800

    feat(@marubase/container): add constructor binding implementation

---
+++ packages/container/source/bindings/constructor-binding.ts
@@ -0,0 +1,21 @@
+import { BindingContract } from "../contracts/binding.contract.js";
+import { CacheContract } from "../contracts/cache.contract.js";
+import { ResolverContract } from "../contracts/resolver.contract.js";
+import { BaseBinding } from "./base-binding.js";
+
+export class ConstructorBinding extends BaseBinding implements BindingContract {
+  public constructor(resolver: ResolverContract, public target: Function) {
+    super(resolver);
+  }
+
+  public resolve<Result>(
+    cache: CacheContract,
+    ...args: Array<unknown>
+  ): Result | undefined {
+    const target = this.target as Constructor<Result>;
+    const targetArgs = this.resolveDependencies(cache).concat(args);
+    return new target(...targetArgs);
+  }
+}
+
+export type Constructor<Result> = new (...args: Array<unknown>) => Result;
