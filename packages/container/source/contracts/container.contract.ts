1fbadece6d5d4c17390617558a378c44f0701273
Endy Jasmi <endyjasmi@gmail.com>
2022-01-08 22:06:00 +0800

    feat(@marubase/container): update container contract

--- packages/container/source/contracts/container.contract.ts
+++ packages/container/source/contracts/container.contract.ts
@@ -7,13 +7,36 @@ import {
 } from "./resolver.contract.js";
 
 export interface ContainerContract {
+  readonly booted: boolean;
+
   readonly cache: CacheContract;
 
   readonly resolver: ResolverContract;
 
+  readonly services: [ServiceName, ServiceProvider][];
+
   bind(bindable: Bindable): ResolverBinding;
+
+  boot(): Promise<void>;
 
   fork(): this;
 
+  install(name: ServiceName, service: ServiceProvider): this;
+
+  installed(name: ServiceName): boolean;
+
   resolve<Result>(resolvable: Resolvable, ...args: unknown[]): Result;
+
+  shutdown(): Promise<void>;
+
+  uninstall(name: ServiceName): this;
 }
+
+export type ServiceName = string | symbol;
+
+export type ServiceProvider = {
+  onBoot?(container: ContainerContract): Promise<void>;
+  onInstall?(container: ContainerContract): void;
+  onShutdown?(container: ContainerContract): Promise<void>;
+  onUninstall?(container: ContainerContract): void;
+};
