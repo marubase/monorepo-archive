fb5e46a110e0fa78b7275927c591cb5de106b4eb
Endy Jasmi <endyjasmi@gmail.com>
2022-01-09 10:08:57 +0800

    refactor(@marubase/container): remove resolver error

--- packages/container/source/errors/index.ts
+++ packages/container/source/errors/index.ts
@@ -1,3 +1,2 @@
 export * from "./base.error.js";
 export * from "./binding.error.js";
-export * from "./resolver.error.js";
--- packages/container/source/errors/resolver.error.ts
+++
@@ -1,3 +0,0 @@
-import { BaseError } from "./base.error.js";
-
-export class ResolverError extends BaseError {}
