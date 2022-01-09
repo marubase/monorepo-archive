b9d313880542601198f7c2e3a3ddc454b4254594
Endy Jasmi <endyjasmi@gmail.com>
2022-01-09 11:18:41 +0800

    test(@marubase/container): add constant binding test

---
+++ packages/container/source/bindings/constant-binding.test.ts
@@ -0,0 +1,27 @@
+import { expect } from "chai";
+import { instance, mock } from "ts-mockito";
+import { CacheContract } from "../contracts/cache.contract.js";
+import { ResolverContract } from "../contracts/resolver.contract.js";
+import { ConstantBinding } from "./constant-binding.js";
+
+suite("ConstantBinding", function () {
+  let mockCache: CacheContract;
+  let mockResolver: ResolverContract;
+  let binding: ConstantBinding;
+  let cache: CacheContract;
+  let resolver: ResolverContract;
+  setup(async function () {
+    mockCache = mock<CacheContract>();
+    mockResolver = mock<ResolverContract>();
+    cache = instance(mockCache);
+    resolver = instance(mockResolver);
+    binding = new ConstantBinding(resolver, true);
+  });
+
+  suite("#resolve(cache, ...args)", function () {
+    test("should return constant", async function () {
+      const instance = binding.resolve(cache);
+      expect(instance).to.equal(true);
+    });
+  });
+});
