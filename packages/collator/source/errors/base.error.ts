999eef3e65d3be4fdca5ff66e9429a9d9faf470f
Endy Jasmi <endyjasmi@gmail.com>
2022-01-10 08:37:43 +0800

    feat(@marubase/collator): add collator error

---
+++ packages/collator/source/errors/base.error.ts
@@ -0,0 +1,6 @@
+export class BaseError extends Error {
+  public constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
---
+++ packages/collator/source/errors/collator.error.ts
@@ -0,0 +1,3 @@
+import { BaseError } from "./base.error.js";
+
+export class CollatorError extends BaseError {}
