7db6558c36a6839aba2a2d591c2a81a4b01e8259
Endy Jasmi <endyjasmi@gmail.com>
2022-01-10 08:45:29 +0800

    feat(@marubase/collator): add collator implementation

---
+++ packages/collator/source/collator.ts
@@ -0,0 +1,110 @@
+import { ComplexCodec } from "./codecs/complex-codec.js";
+import { CollatorError } from "./errors/collator.error.js";
+import { FloatValue } from "./values/float-value.js";
+import { IntegerValue } from "./values/integer-value.js";
+import { MetaValue } from "./values/meta-value.js";
+import { VersionStamp } from "./values/version-stamp.js";
+
+export class Collator {
+  protected codec: ComplexCodec;
+
+  public constructor(codec: ComplexCodec) {
+    this.codec = codec;
+  }
+
+  public asc(value: unknown): MetaValue {
+    return MetaValue.create(value, true);
+  }
+
+  public decode(binary: Buffer): unknown {
+    return this.codec.decode(binary);
+  }
+
+  public desc(value: unknown): MetaValue {
+    return MetaValue.create(value, false);
+  }
+
+  public encode(value: unknown): Buffer | { buffer: Buffer; position: number } {
+    const meta = MetaValue.create(value, true);
+    const binaries = this.codec.encode([], meta);
+
+    const encoding = { size: 0, versionstamped: false } as {
+      position?: number;
+      size: number;
+      versionstamped: boolean;
+    };
+    binaries.forEach((binary) => {
+      if (Buffer.isBuffer(binary)) return (encoding.size += binary.length);
+      if (encoding.versionstamped) {
+        const context = `Encoding value.`;
+        const problem = `Value have already been versionstamped.`;
+        const solution = `Please make sure value only have single versionstamp.`;
+        throw new CollatorError(`${context} ${problem} ${solution}`);
+      }
+      encoding.position = encoding.size;
+      encoding.size += binary.buffer.length;
+      encoding.versionstamped = true;
+    });
+
+    const encoded = Buffer.allocUnsafe(encoding.size);
+    for (let index = 0, offset = 0; index < binaries.length; index++) {
+      const binary = binaries[index];
+      if (Buffer.isBuffer(binary)) {
+        encoded.set(binary, offset);
+        offset += binary.length;
+      } else {
+        encoded.set(binary.buffer, offset);
+        offset += binary.buffer.length;
+      }
+    }
+    return typeof encoding.position === "number"
+      ? { buffer: encoded, position: encoding.position + 1 }
+      : encoded;
+  }
+
+  public float32(value: unknown): FloatValue {
+    return FloatValue.create(value, "float32");
+  }
+
+  public float64(value: unknown): FloatValue {
+    return FloatValue.create(value, "float64");
+  }
+
+  public int16(value: unknown): IntegerValue {
+    return IntegerValue.create(value, "int16");
+  }
+
+  public int32(value: unknown): IntegerValue {
+    return IntegerValue.create(value, "int32");
+  }
+
+  public int64(value: unknown): IntegerValue {
+    return IntegerValue.create(value, "int64");
+  }
+
+  public int8(value: unknown): IntegerValue {
+    return IntegerValue.create(value, "int8");
+  }
+
+  public uint16(value: unknown): IntegerValue {
+    return IntegerValue.create(value, "uint16");
+  }
+
+  public uint32(value: unknown): IntegerValue {
+    return IntegerValue.create(value, "uint32");
+  }
+
+  public uint64(value: unknown): IntegerValue {
+    return IntegerValue.create(value, "uint64");
+  }
+
+  public uint8(value: unknown): IntegerValue {
+    return IntegerValue.create(value, "uint8");
+  }
+
+  public versionstamp(code: Buffer | number = 0, value?: Buffer): VersionStamp {
+    return Buffer.isBuffer(code)
+      ? VersionStamp.create(code.readUInt16BE(10), code.subarray(0, 10))
+      : VersionStamp.create(code, value);
+  }
+}
---
+++ packages/collator/source/index.ts
@@ -0,0 +1,46 @@
+import { CodeTable } from "./code-table.js";
+import { BooleanCodec } from "./codecs/boolean-codec.js";
+import { BufferCodec } from "./codecs/buffer-codec.js";
+import { ComplexCodec } from "./codecs/complex-codec.js";
+import { DateCodec } from "./codecs/date-codec.js";
+import { FloatCodec } from "./codecs/float-codec.js";
+import { IntegerCodec } from "./codecs/integer-codec.js";
+import { StringCodec } from "./codecs/string-codec.js";
+import { VersionCodec } from "./codecs/version-codec.js";
+import { Collator } from "./collator.js";
+
+const codec = new ComplexCodec(CodeTable);
+codec.register(BooleanCodec.service);
+codec.register(IntegerCodec.service);
+codec.register(FloatCodec.service);
+codec.register(DateCodec.service);
+codec.register(BufferCodec.service);
+codec.register(StringCodec.service);
+codec.register(VersionCodec.service);
+
+const collator = new Collator(codec);
+export default collator;
+
+export const decode = collator.decode.bind(collator);
+export const encode = collator.encode.bind(collator);
+
+export const asc = collator.asc.bind(collator);
+export const desc = collator.desc.bind(collator);
+
+export const float32 = collator.float32.bind(collator);
+export const float64 = collator.float64.bind(collator);
+export const int8 = collator.int8.bind(collator);
+export const int16 = collator.int16.bind(collator);
+export const int32 = collator.int32.bind(collator);
+export const int64 = collator.int64.bind(collator);
+export const uint8 = collator.uint8.bind(collator);
+export const uint16 = collator.uint16.bind(collator);
+export const uint32 = collator.uint32.bind(collator);
+export const uint64 = collator.uint64.bind(collator);
+export const versionstamp = collator.versionstamp.bind(collator);
+
+export * from "./code-table.js";
+export * as codecs from "./codecs/index.js";
+export * from "./collator.js";
+export * as errors from "./errors/index.js";
+export * as values from "./values/index.js";
