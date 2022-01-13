eb76aed7f9522a0bbe194792eff6e9d13ee6ccfe
Endy Jasmi <endyjasmi@gmail.com>
2022-01-13 14:42:08 +0800

    refactor(@marubase/collator): update meta value implementation

--- packages/collator/source/values/meta-value.ts
+++ packages/collator/source/values/meta-value.ts
@@ -1,6 +1,6 @@
 import { FloatValue } from "./float-value.js";
 import { IntegerValue } from "./integer-value.js";
-import { VersionStamp } from "./version-stamp.js";
+import { VersionstampValue } from "./versionstamp-value.js";
 
 export class MetaValue {
   public static create(value: unknown, ascending: boolean): MetaValue {
@@ -9,23 +9,15 @@ export class MetaValue {
       : value;
   }
 
-  public ascending: boolean;
-
   public type: string;
 
-  public value: unknown;
-
-  public constructor(value: unknown, ascending: boolean) {
-    this.ascending = ascending;
-    this.value = value;
-
-    if (typeof value !== "object") this.type = typeof value;
+  public constructor(public value: unknown, public ascending: boolean) {
     if (Array.isArray(value)) this.type = "array";
     else if (Buffer.isBuffer(value)) this.type = "buffer";
     else if (value instanceof Date) this.type = "date";
     else if (value instanceof FloatValue) this.type = "float";
     else if (value instanceof IntegerValue) this.type = "integer";
-    else if (value instanceof VersionStamp) this.type = "versionstamp";
+    else if (value instanceof VersionstampValue) this.type = "versionstamp";
     else if (value === null) this.type = "null";
     else this.type = typeof value;
   }
