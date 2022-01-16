664b587f02021677c92d48ea341cd6ed4df4b49b
Endy Jasmi <endyjasmi@gmail.com>
2022-01-16 21:40:05 +0800

    feat(@marubase/storage-fdb): add write transaction implementation

--- packages/storage-fdb/source/index.ts
+++ packages/storage-fdb/source/index.ts
@@ -2,3 +2,4 @@ export * from "./range-iterable.js";
 export * from "./read-bucket.js";
 export * from "./read-transaction.js";
 export * from "./write-bucket.js";
+export * from "./write-transaction.js";
