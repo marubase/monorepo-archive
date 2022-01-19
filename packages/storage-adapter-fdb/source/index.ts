93c10fb35a3bc255d9d8b072ad816fdc14fcb954
Endy Jasmi <endyjasmi@gmail.com>
2022-01-19 13:22:05 +0800

    refactor(@marubase/storage-adapter-fdb): add read transaction implementation

--- packages/storage-adapter-fdb/source/index.ts
+++ packages/storage-adapter-fdb/source/index.ts
@@ -1,2 +1,3 @@
 export * from "./range-iterable.js";
 export * from "./read-bucket.js";
+export * from "./read-transaction.js";
---
+++ packages/storage-adapter-fdb/source/read-transaction.ts
@@ -0,0 +1,65 @@
+import {
+  asc,
+  desc,
+  float32,
+  float64,
+  int16,
+  int32,
+  int64,
+  int8,
+  uint16,
+  uint32,
+  uint64,
+  uint8,
+} from "@marubase/collator";
+import {
+  ReadBucketContract,
+  ReadTransactionContract,
+  StorageError,
+  StorageFactory,
+} from "@marubase/storage-adapter";
+import { Transaction } from "foundationdb";
+
+export class ReadTransaction implements ReadTransactionContract {
+  public readonly cast = {
+    float32,
+    float64,
+    int16,
+    int32,
+    int64,
+    int8,
+    uint16,
+    uint32,
+    uint64,
+    uint8,
+  };
+
+  public readonly order = { asc, desc };
+
+  protected _bucketNames: string[];
+
+  protected _factory: StorageFactory;
+
+  protected _fdbTransaction: Transaction;
+
+  public constructor(
+    fdbTransaction: Transaction,
+    bucketNames: string[],
+    factory: StorageFactory,
+  ) {
+    this._fdbTransaction = fdbTransaction;
+    this._bucketNames = bucketNames;
+    this._factory = factory;
+  }
+
+  public bucket(bucketName: string): ReadBucketContract {
+    if (this._bucketNames.indexOf(bucketName) < 0) {
+      const scopes = this._bucketNames.join(", ");
+      const context = `Running read transaction in "${bucketName}".`;
+      const problem = `Transaction out of scope.`;
+      const solution = `Please run transaction in ${scopes}.`;
+      throw new StorageError(`${context} ${problem} ${solution}`);
+    }
+    return this._factory.createReadBucket(this._fdbTransaction, bucketName);
+  }
+}
