4d52cac3aed17285d09f886fadf6bcf7817dd0f3
Endy Jasmi <endyjasmi@gmail.com>
2022-01-20 11:41:15 +0800

    test(@marubase/storage-adapter): add storage test

---
+++ packages/storage-adapter/source/storage.test.ts
@@ -0,0 +1,14 @@
+import { bucketTest } from "./bucket.test.js";
+import { concurrencyTest } from "./concurrency.test.js";
+import { StorageContract } from "./contracts/storage.contract.js";
+import { rangeTest } from "./range.test.js";
+import { transactionTest } from "./transaction.test.js";
+import { versionstampTest } from "./versionstamp.test.js";
+
+export function storageTest(storageFn: () => StorageContract): void {
+  bucketTest(storageFn);
+  concurrencyTest(storageFn);
+  rangeTest(storageFn);
+  transactionTest(storageFn);
+  versionstampTest(storageFn);
+}
