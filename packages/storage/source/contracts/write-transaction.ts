44e9cd1b1dfa0440a510ac6699da1be95c1e5ce1
Endy Jasmi <endyjasmi@gmail.com>
2022-01-21 22:03:38 +0800

    feat(@marubase/storage): update write transaction contract

--- packages/storage/source/contracts/write-transaction.ts
+++ packages/storage/source/contracts/write-transaction.ts
@@ -1,8 +1,11 @@
 import { versionstamp } from "@marubase/collator";
 import { ReadTransactionContract } from "./read-transaction.js";
+import { StorageFactory } from "./storage.contract.js";
 import { WriteBucketContract } from "./write-bucket.js";
 
 export interface WriteTransactionContract extends ReadTransactionContract {
+  readonly factory: StorageFactory;
+
   readonly versionstamp: typeof versionstamp;
 
   bucket(name: string): WriteBucketContract;
