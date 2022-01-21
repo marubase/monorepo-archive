1655651a14cb716af0fe7a7887c404ef41b71874
Endy Jasmi <endyjasmi@gmail.com>
2022-01-21 23:04:27 +0800

    refactor(@marubase/storage): update write bucket contract

--- packages/storage/source/contracts/write-bucket.ts
+++ packages/storage/source/contracts/write-bucket.ts
@@ -1,10 +1,7 @@
 import { ReadBucketContract } from "./read-bucket.js";
-import { StorageFactory } from "./storage.contract.js";
 import { WriteTransactionContract } from "./write-transaction.js";
 
 export interface WriteBucketContract extends ReadBucketContract {
-  readonly factory: StorageFactory;
-
   readonly transaction: WriteTransactionContract;
 
   clear(key: unknown): void;
