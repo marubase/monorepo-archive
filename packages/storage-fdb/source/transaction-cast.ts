acf6de299ef09e06775d4b623759944f6d36d0cb
Endy Jasmi <endyjasmi@gmail.com>
2022-01-22 10:33:52 +0800

    feat(@marubase/storage-fdb): add transaction cast implementation

---
+++ packages/storage-fdb/source/transaction-cast.ts
@@ -0,0 +1,26 @@
+import {
+  float32,
+  float64,
+  int16,
+  int32,
+  int64,
+  int8,
+  uint16,
+  uint32,
+  uint64,
+  uint8,
+} from "@marubase/collator";
+import { TransactionCast } from "@marubase/storage";
+
+export const cast: TransactionCast = {
+  float32,
+  float64,
+  int16,
+  int32,
+  int64,
+  int8,
+  uint16,
+  uint32,
+  uint64,
+  uint8,
+};
