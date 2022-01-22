7f4399e6ab45f1952aa993aef3b50be52727a8e6
Endy Jasmi <endyjasmi@gmail.com>
2022-01-22 22:18:33 +0800

    chore: remove @marubase/storage-adapter-lmdb module

--- packages/storage-adapter-lmdb/source/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./mutation-counter.js";
-export * from "./range-iterable.js";
-export * from "./read-bucket.js";
-export * from "./read-transaction.js";
-export * from "./storage.js";
-export * from "./write-bucket.js";
-export * from "./write-transaction.js";
