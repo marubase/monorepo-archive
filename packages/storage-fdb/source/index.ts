ebdccb2a5bf202e7da0ca0d79105e34a455dd7ad
Endy Jasmi <endyjasmi@gmail.com>
2022-01-22 22:11:11 +0800

    feat(@marubase/storage-fdb): update output index

--- packages/storage-fdb/source/index.ts
+++ packages/storage-fdb/source/index.ts
@@ -2,5 +2,7 @@ export * from "./range-iterable.js";
 export * from "./read-bucket.js";
 export * from "./read-transaction.js";
 export * from "./storage.js";
+export * from "./transaction-cast.js";
+export * from "./transaction-order.js";
 export * from "./write-bucket.js";
 export * from "./write-transaction.js";
