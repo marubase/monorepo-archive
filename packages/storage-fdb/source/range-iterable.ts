a7bbe63ab594622bf4361d1d00773204d4e46f0a
Endy Jasmi <endyjasmi@gmail.com>
2022-01-22 20:55:16 +0800

    feat(@marubase/storage-fdb): update range iterable implementation

--- packages/storage-fdb/source/range-iterable.ts
+++ packages/storage-fdb/source/range-iterable.ts
@@ -1,23 +1,23 @@
 import { decode } from "@marubase/collator";
 
-export class RangeIterable implements AsyncIterable<[unknown, unknown]> {
+export class RangeIterable<Key, Value> implements AsyncIterable<[Key, Value]> {
   protected _fdbRange: AsyncGenerator<[Buffer, Buffer]>;
 
   public constructor(fdbRange: AsyncGenerator<[Buffer, Buffer]>) {
     this._fdbRange = fdbRange;
   }
 
-  public [Symbol.asyncIterator](): AsyncIterator<[unknown, unknown]> {
+  public [Symbol.asyncIterator](): AsyncIterator<[Key, Value]> {
     return this;
   }
 
-  public async next(): Promise<IteratorResult<[unknown, unknown]>> {
+  public async next(): Promise<IteratorResult<[Key, Value]>> {
     const fdbCursor = await this._fdbRange.next();
     if (fdbCursor.done) return { done: true, value: undefined };
 
     const [fdbKey, fdbValue] = fdbCursor.value;
-    const key = decode(fdbKey);
-    const value = decode(fdbValue);
+    const key = decode(fdbKey) as Key;
+    const value = decode(fdbValue) as Value;
     return { done: false, value: [key, value] };
   }
 }
