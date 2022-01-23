16a4f407e0b2f668d1aeebd067b3e765d8c2aa67
Endy Jasmi <endyjasmi@gmail.com>
2022-01-23 20:37:47 +0800

    refactor(@marubase/storage-lmdb): update write bucket implementation

--- packages/storage-lmdb/source/write-bucket.ts
+++ packages/storage-lmdb/source/write-bucket.ts
@@ -1,19 +1,24 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { encode } from "@marubase/collator";
+import { decode, encode } from "@marubase/collator";
 import {
+  RangeOptions,
   StorageError,
   StorageFactory,
   WriteBucketContract,
   WriteTransactionContract,
 } from "@marubase/storage";
-import { Database, RangeOptions } from "lmdb";
-import { ReadBucket } from "./read-bucket.js";
+import { Database, RangeOptions as LMDBRangeOptions } from "lmdb";
 
 export class WriteBucket<Key, Value>
-  extends ReadBucket<Key, Value>
   implements WriteBucketContract<Key, Value>
 {
+  public readonly factory: StorageFactory;
+
+  public readonly name: string;
+
   public readonly transaction: WriteTransactionContract;
+
+  protected _lmdbDatabase: Database<Buffer, Buffer>;
 
   public constructor(
     factory: StorageFactory,
@@ -21,8 +26,10 @@ export class WriteBucket<Key, Value>
     name: string,
     lmdbDatabase: Database<Buffer, Buffer>,
   ) {
-    super(factory, transaction, name, lmdbDatabase);
+    this.factory = factory;
     this.transaction = transaction;
+    this.name = name;
+    this._lmdbDatabase = lmdbDatabase;
   }
 
   public clear(key: Key): void {
@@ -34,7 +41,7 @@ export class WriteBucket<Key, Value>
   }
 
   public clearRange(start: Key, end: Key): void {
-    const lmdbOptions: RangeOptions = {};
+    const lmdbOptions: LMDBRangeOptions = {};
     const encodedStart = encode([this.name, start]);
     lmdbOptions.start = !Buffer.isBuffer(encodedStart)
       ? encodedStart.buffer
@@ -47,6 +54,40 @@ export class WriteBucket<Key, Value>
     this._lmdbDatabase
       .getRange(lmdbOptions)
       .forEach(({ key: lmdbKey }) => this._lmdbDatabase.remove(lmdbKey));
+  }
+
+  public async get(key: Key, defaultValue?: Value): Promise<Value | undefined> {
+    const encodedKey = encode([this.name, key]);
+    const lmdbKey = !Buffer.isBuffer(encodedKey)
+      ? encodedKey.buffer
+      : encodedKey;
+    const lmdbValue = this._lmdbDatabase.getBinary(lmdbKey);
+    return typeof lmdbValue !== "undefined"
+      ? (decode(lmdbValue) as Value)
+      : defaultValue;
+  }
+
+  public getRange(
+    start: Key,
+    end: Key,
+    options?: RangeOptions,
+  ): AsyncIterable<[Key, Value]> {
+    const lmdbOptions = Object.assign({}, options) as LMDBRangeOptions;
+    if (typeof lmdbOptions.limit !== "number") lmdbOptions.limit = Infinity;
+    if (typeof lmdbOptions.reverse !== "boolean") lmdbOptions.reverse = false;
+
+    const encodedStart = encode([this.name, start]);
+    lmdbOptions.start = !Buffer.isBuffer(encodedStart)
+      ? encodedStart.buffer
+      : encodedStart;
+
+    const encodedEnd = encode([this.name, end]);
+    lmdbOptions.end = !Buffer.isBuffer(encodedEnd)
+      ? encodedEnd.buffer
+      : encodedEnd;
+
+    const lmdbRange = this._lmdbDatabase.getRange(lmdbOptions);
+    return this.factory.createRangeIterable(lmdbRange);
   }
 
   public set(key: Key, value: Value): void {
