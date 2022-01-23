1d87b95a691b38b071ded10443724fe12360feee
Endy Jasmi <endyjasmi@gmail.com>
2022-01-23 13:56:27 +0800

    chore(@marubase/storage-idb): initial commit

---
+++ packages/storage-idb/webpack.config.cjs
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = Object.assign(require("../../webpack.config.js"), {
+  devtool: "source-map",
+  entry: path.join(process.cwd(), "source", "index.ts"),
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    library: { name: ["Marubase", "StorageIDB"], type: "assign-properties" },
+    path: path.join(process.cwd(), "build"),
+  },
+});
