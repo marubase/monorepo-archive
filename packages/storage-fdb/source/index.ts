8be86ea3ddc4b24dec0329b077cc5fdf8892e61d
Endy Jasmi <endyjasmi@gmail.com>
2022-01-24 21:25:22 +0800

    refactor(@marubase/storage-fdb): update index.js

--- packages/storage-fdb/source/index.ts
+++ packages/storage-fdb/source/index.ts
@@ -1,3 +1,5 @@
+export * from "@marubase/collator";
+export * from "@marubase/storage";
 export * from "./range-iterable.js";
 export * from "./read-bucket.js";
 export * from "./read-transaction.js";
