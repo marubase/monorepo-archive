07108efbe72565601a2ae70836aa8cbe473c2001
Endy Jasmi <endyjasmi@gmail.com>
2022-01-28 20:21:27 +0800

    feat(@marubase/container): add key resolver implementation

---
+++ packages/container/source/resolvers/key-resolver.ts
@@ -0,0 +1,35 @@
+import { RegistryContract, RegistryKey } from "../contracts/registry.js";
+import { ResolverContract } from "../contracts/resolver.js";
+import { ScopeContract } from "../contracts/scope.js";
+import { ContainerError } from "../index.js";
+import { BaseResolver } from "./base-resolver.js";
+
+export class KeyResolver extends BaseResolver implements ResolverContract {
+  protected _key: RegistryKey;
+
+  public constructor(registry: RegistryContract, key: RegistryKey) {
+    super(registry);
+    this._key = key;
+  }
+
+  public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
+    const resolver = this._registry.getResolverByKey(this._key);
+    if (typeof resolver === "undefined") {
+      const contextKey =
+        typeof this._key === "symbol" ? this._key.toString() : this._key;
+      const context = `Resolving instance registered at '${contextKey}'.`;
+      const problem = `Resolver not found.`;
+      const solution = `Please try another key.`;
+      throw new ContainerError(`${context} ${problem} ${solution}`);
+    }
+
+    if (resolver.scope === "transient") return resolver.resolve(scope, ...args);
+
+    const cache = scope[resolver.scope];
+    return cache.has(this._key)
+      ? (cache
+          .set(this._key, resolver.resolve(scope, ...args))
+          .get(this._key) as Result)
+      : (cache.get(this._key) as Result);
+  }
+}
