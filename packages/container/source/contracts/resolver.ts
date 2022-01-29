1e80644e01867a787c3749dd03d281755a3ecc10
Endy Jasmi <endyjasmi@gmail.com>
2022-01-29 22:13:45 +0800

    feat(@marubase/container): update resolver contract

--- packages/container/source/contracts/resolver.ts
+++ packages/container/source/contracts/resolver.ts
@@ -1,14 +1,15 @@
 import {
+  BindingKey,
+  BindingTag,
   RegistryContract,
-  RegistryKey,
-  RegistryTag,
-  RegistryTags,
   Resolvable,
 } from "./registry.js";
 import { ScopeContract } from "./scope.js";
 
 export interface ResolverContract {
-  readonly bindingKey?: RegistryKey;
+  readonly bindingKey?: BindingKey;
+
+  readonly bindingTags: BindingTag[];
 
   readonly dependencies: Resolvable[];
 
@@ -16,29 +17,27 @@ export interface ResolverContract {
 
   readonly scope: ResolverScope;
 
-  readonly tags: RegistryTags;
-
   clearBindingKey(): this;
 
+  clearBindingTag(bindingTag: BindingTag): this;
+
+  clearBindingTags(): this;
+
   clearDependencies(): this;
-
-  clearTag(tag: RegistryTag): this;
-
-  clearTags(): this;
 
   resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result;
 
   resolveDependencies(scope: ScopeContract): unknown[];
 
-  setBindingKey(bindingKey: RegistryKey): this;
+  setBindingKey(bindingKey: BindingKey): this;
+
+  setBindingTag(tag: BindingTag): this;
+
+  setBindingTags(tags: BindingTag[]): this;
 
   setDependencies(dependencies: Resolvable[]): this;
 
   setScope(scope: ResolverScope): this;
-
-  setTag(tag: RegistryTag): this;
-
-  setTags(tags: RegistryTags): this;
 }
 
 export type ResolverScope = "container" | "request" | "singleton" | "transient";
