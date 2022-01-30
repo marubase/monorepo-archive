6f834f09ce17f4985ef42cc5a36b5fb393be2861
Endy Jasmi <endyjasmi@gmail.com>
2022-01-30 13:08:20 +0800

    feat(@marubase/container): add alias resolver implementation

---
+++ packages/container/source/resolvers/alias-resolver.ts
@@ -0,0 +1,31 @@
+import {
+  Bindable,
+  BindingKey,
+  BindingRoot,
+  RegistryContract,
+} from "../contracts/registry.js";
+import { ResolverContract } from "../contracts/resolver.js";
+import { ScopeContract } from "../contracts/scope.js";
+import { BaseResolver } from "./base-resolver.js";
+
+export class AliasResolver extends BaseResolver implements ResolverContract {
+  protected _alias: Bindable;
+
+  public constructor(registry: RegistryContract, alias: Bindable) {
+    super(registry);
+    this._alias = alias;
+  }
+
+  public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
+    let resolveKey = !Array.isArray(scope.resolvable)
+      ? ([scope.resolvable, BindingRoot] as BindingKey)
+      : (scope.resolvable as BindingKey);
+    if (typeof scope.resolvable === "string") {
+      const pattern = /^([\p{Alpha}\p{N}]+)#([\p{Alpha}\p{N}]+)$/u;
+      const matched = scope.resolvable.match(pattern);
+      if (matched) resolveKey = [matched[1], matched[2]];
+    }
+    const aliasKey = [this._alias, resolveKey[1]] as BindingKey;
+    return this._registry.resolve(scope, aliasKey, ...args);
+  }
+}
--- packages/container/source/resolvers/index.ts
+++ packages/container/source/resolvers/index.ts
@@ -1,3 +1,4 @@
+export * from "./alias-resolver.js";
 export * from "./base-resolver.js";
 export * from "./class-resolver.js";
 export * from "./constant-resolver.js";
