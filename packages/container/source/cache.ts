5c93a666adbe46538934d8b3887f16b31ee6cc0f
Endy Jasmi <endyjasmi@gmail.com>
2022-01-30 11:58:11 +0800

    refactor(@marubase/container): update cache implementation

--- packages/container/source/cache.ts
+++ packages/container/source/cache.ts
@@ -7,45 +7,24 @@ export class Cache implements CacheContract {
     this._store = store || new Map<CacheToken, Map<CacheToken, unknown>>();
   }
 
-  public clear(): this {
-    this._store = new Map<CacheToken, Map<CacheToken, unknown>>();
-    return this;
-  }
-
-  public delete(key: CacheKey): this {
-    if (!Array.isArray(key)) key = [key, Symbol.for("undefined")];
-    const [primary, secondary] = key;
+  public clear([primary, secondary]: CacheKey): this {
     const table = this._store.get(primary) || new Map<CacheToken, unknown>();
     table.delete(secondary);
     if (table.size < 1) this._store.delete(primary);
     return this;
   }
 
-  public fork(): this {
-    const newStore = new Map<CacheToken, Map<CacheToken, unknown>>();
-    for (const [token, table] of this._store)
-      newStore.set(token, new Map(table));
-    const Static = this.constructor as typeof Cache;
-    return new Static(newStore) as this;
-  }
-
-  public get(key: CacheKey): unknown {
-    if (!Array.isArray(key)) key = [key, Symbol.for("undefined")];
-    const [primary, secondary] = key;
+  public get([primary, secondary]: CacheKey): unknown {
     const table = this._store.get(primary);
     return typeof table !== "undefined" ? table.get(secondary) : undefined;
   }
 
-  public has(key: CacheKey): boolean {
-    if (!Array.isArray(key)) key = [key, Symbol.for("undefined")];
-    const [primary, secondary] = key;
+  public has([primary, secondary]: CacheKey): boolean {
     const table = this._store.get(primary);
     return typeof table !== "undefined" ? table.has(secondary) : false;
   }
 
-  public set(key: CacheKey, value: unknown): this {
-    if (!Array.isArray(key)) key = [key, Symbol.for("undefined")];
-    const [primary, secondary] = key;
+  public set([primary, secondary]: CacheKey, value: unknown): this {
     const table = this._store.get(primary) || new Map<CacheToken, unknown>();
     table.set(secondary, value);
     this._store.set(primary, table);
