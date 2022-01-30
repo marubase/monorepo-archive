4390c84dbf972401b40e3ab52cfdffea8f398d70
Endy Jasmi <endyjasmi@gmail.com>
2022-01-30 18:36:52 +0800

    refactor(@marubase/container): update cache implementation

--- packages/container/source/cache.ts
+++ packages/container/source/cache.ts
@@ -1,30 +1,30 @@
 import { CacheContract, CacheKey, CacheToken } from "./contracts/cache.js";
 
 export class Cache implements CacheContract {
-  protected _store: Map<CacheToken, Map<CacheToken, unknown>>;
+  protected _store = new Map<CacheToken, Map<CacheToken, unknown>>();
 
-  public constructor(store?: Map<CacheToken, Map<CacheToken, unknown>>) {
-    this._store = store || new Map<CacheToken, Map<CacheToken, unknown>>();
-  }
-
-  public clear([primary, secondary]: CacheKey): this {
+  public clear(key: CacheKey): this {
+    const [primary, secondary] = key;
     const table = this._store.get(primary) || new Map<CacheToken, unknown>();
     table.delete(secondary);
     if (table.size < 1) this._store.delete(primary);
     return this;
   }
 
-  public get([primary, secondary]: CacheKey): unknown {
+  public get(key: CacheKey): unknown {
+    const [primary, secondary] = key;
     const table = this._store.get(primary);
     return typeof table !== "undefined" ? table.get(secondary) : undefined;
   }
 
-  public has([primary, secondary]: CacheKey): boolean {
+  public has(key: CacheKey): boolean {
+    const [primary, secondary] = key;
     const table = this._store.get(primary);
     return typeof table !== "undefined" ? table.has(secondary) : false;
   }
 
-  public set([primary, secondary]: CacheKey, value: unknown): this {
+  public set(key: CacheKey, value: unknown): this {
+    const [primary, secondary] = key;
     const table = this._store.get(primary) || new Map<CacheToken, unknown>();
     table.set(secondary, value);
     this._store.set(primary, table);
