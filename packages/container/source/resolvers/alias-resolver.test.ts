6d85f68561e072b1842659ea4a840612d900f07f
Endy Jasmi <endyjasmi@gmail.com>
2022-01-30 22:13:06 +0800

    test(@marubase/container): add alias resolver test

---
+++ packages/container/source/resolvers/alias-resolver.test.ts
@@ -0,0 +1,54 @@
+import { expect } from "chai";
+import { anything, instance, mock, reset, when } from "ts-mockito";
+import { RegistryContract } from "../contracts/registry.js";
+import { ScopeContract } from "../contracts/scope.js";
+import { AliasResolver } from "./alias-resolver.js";
+
+describe("AliasResolver", function () {
+  let mockRegistry: RegistryContract;
+  let mockScope: ScopeContract;
+  let registry: RegistryContract;
+  let resolver: AliasResolver;
+  let scope: ScopeContract;
+  beforeEach(async function () {
+    mockRegistry = mock();
+    mockScope = mock();
+    registry = instance(mockRegistry);
+    scope = instance(mockScope);
+    resolver = new AliasResolver(registry, "alias");
+  });
+  afterEach(async function () {
+    reset(mockRegistry);
+    reset(mockScope);
+  });
+
+  describe("#resolve(scope, ...args)", function () {
+    context("when scope resolvable is 'test''", function () {
+      it("should return resolved instance", async function () {
+        when(mockScope.resolvable).thenReturn("test");
+        when(mockRegistry.resolve<boolean>(scope, anything())).thenReturn(true);
+
+        const instance = resolver.resolve(scope);
+        expect(instance).to.be.true;
+      });
+    });
+    context("when scope resolvable is 'test#method'", function () {
+      it("should return resolved instance", async function () {
+        when(mockScope.resolvable).thenReturn("test#method");
+        when(mockRegistry.resolve<boolean>(scope, anything())).thenReturn(true);
+
+        const instance = resolver.resolve(scope);
+        expect(instance).to.be.true;
+      });
+    });
+    context("when scope resolvable is ['test', 'method']", function () {
+      it("should return resolved instance", async function () {
+        when(mockScope.resolvable).thenReturn(["test", "method"]);
+        when(mockRegistry.resolve<boolean>(scope, anything())).thenReturn(true);
+
+        const instance = resolver.resolve(scope);
+        expect(instance).to.be.true;
+      });
+    });
+  });
+});
