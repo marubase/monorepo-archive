ac473640291e6062cbd99da826cd50ed27baa6f6
Endy Jasmi <endyjasmi@gmail.com>
2022-01-31 01:03:18 +0800

    test(@marubase/container): add registry bind test

---
+++ packages/container/source/registry-bind.test.ts
@@ -0,0 +1,140 @@
+import { expect } from "chai";
+import "reflect-metadata";
+import { inject, resolvable } from "./decorator.js";
+import { BaseResolver, ContainerError } from "./index.js";
+import { setResolvable } from "./metadata.js";
+import { Registry } from "./registry.js";
+
+@resolvable()
+class Tester {
+  protected _date: Date;
+
+  public constructor(@inject(Date) date: Date) {
+    this._date = date;
+  }
+
+  @resolvable()
+  public [Symbol("test")](@inject("random") random: number): string {
+    return `${this._date.toISOString()}#${random}`;
+  }
+
+  public [Symbol("test2")](random: number): string {
+    return `${this._date.toISOString()}#${random}`;
+  }
+
+  @resolvable()
+  public test(@inject("random") random: number): string {
+    return `${this._date.toISOString()}#${random}`;
+  }
+
+  public test2(random: number): string {
+    return `${this._date.toISOString()}#${random}`;
+  }
+}
+
+describe("Registry (Bind)", function () {
+  let registry: Registry;
+  beforeEach(async function () {
+    registry = new Registry();
+  });
+
+  describe("#bind(bindable).to(class)", function () {
+    it("should return resolver", async function () {
+      const resolver = registry.bind("test").to(Date);
+      expect(resolver).to.be.an.instanceOf(BaseResolver);
+    });
+  });
+
+  describe("#bind(bindable).toAlias(alias)", function () {
+    it("should return resolver", async function () {
+      const resolver = registry.bind("test").toAlias("alias");
+      expect(resolver).to.be.an.instanceOf(BaseResolver);
+    });
+  });
+
+  describe("#bind(bindable).toClass(class)", function () {
+    context("when target is resolvable", function () {
+      it("should return resolver", async function () {
+        const resolver = registry.bind("test").toClass(Tester);
+        expect(resolver).to.be.an.instanceOf(BaseResolver);
+      });
+    });
+    context("when target is not resolvable", function () {
+      it("should return resolver", async function () {
+        const resolver = registry.bind("test").toClass(Date);
+        expect(resolver).to.be.an.instanceOf(BaseResolver);
+      });
+    });
+  });
+
+  describe("#bind(bindable).toConstant(constant)", function () {
+    it("should return resolver", async function () {
+      const resolver = registry.bind("test").toConstant(true);
+      expect(resolver).to.be.an.instanceOf(BaseResolver);
+    });
+  });
+
+  describe("#bind(bindable).toFunction(target)", function () {
+    context("when target is resolvable", function () {
+      it("should return resolver", async function () {
+        const test = (): boolean => true;
+        setResolvable(test);
+        const resolver = registry.bind("test").toFunction(test);
+        expect(resolver).to.be.an.instanceOf(BaseResolver);
+      });
+    });
+    context("when target is not resolvable", function () {
+      it("should return resolver", async function () {
+        const test = (): boolean => true;
+        const resolver = registry.bind("test").toFunction(test);
+        expect(resolver).to.be.an.instanceOf(BaseResolver);
+      });
+    });
+  });
+
+  describe("#bind(bindable).toKey(bindingKey)", function () {
+    it("should return resolver", async function () {
+      const resolver = registry.bind("test").toKey(["test", "method"]);
+      expect(resolver).to.be.an.instanceOf(BaseResolver);
+    });
+  });
+
+  describe("#bind(bindable).toMethod(target, method)", function () {
+    context("when target is resolvable", function () {
+      it("should return resolver", async function () {
+        const resolver = registry
+          .bind("test")
+          .toMethod(Tester.prototype, "test");
+        expect(resolver).to.be.an.instanceOf(BaseResolver);
+      });
+    });
+    context("when target is not resolvable", function () {
+      it("should return resolver", async function () {
+        const resolver = registry.bind("test").toMethod(Date, "now");
+        expect(resolver).to.be.an.instanceOf(BaseResolver);
+      });
+    });
+  });
+
+  describe("#bind(bindable).toSelf()", function () {
+    context("when bindable is class", function () {
+      it("should return resolver", async function () {
+        const resolver = registry.bind(Date).toSelf();
+        expect(resolver).to.be.an.instanceOf(BaseResolver);
+      });
+    });
+    context("when bindable is not class", function () {
+      it("should throw error", async function () {
+        const process = (): unknown => registry.bind("test").toSelf();
+        expect(process).to.throw(ContainerError);
+      });
+    });
+  });
+
+  describe("#bind(bindable).toTag(tag)", function () {
+    it("should return resolver", async function () {
+      const resolver = registry.bind("test").toTag("tag");
+      expect(resolver).to.be.an.instanceOf(BaseResolver);
+    });
+  });
+});
