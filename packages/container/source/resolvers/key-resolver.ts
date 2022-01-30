c78fbc30ac3a2eadd3b7ad05fa3581edce3b2449
Endy Jasmi <endyjasmi@gmail.com>
2022-01-30 11:57:48 +0800

    refactor(@marubase/container): update key resolver implementation

--- packages/container/source/resolvers/key-resolver.ts
+++ packages/container/source/resolvers/key-resolver.ts
@@ -1,7 +1,9 @@
 import {
   BindingKey,
+  BindingRoot,
   BindingToken,
   RegistryContract,
+  Resolvable,
 } from "../contracts/registry.js";
 import { ResolverContract } from "../contracts/resolver.js";
 import { ScopeContract } from "../contracts/scope.js";
@@ -9,15 +11,25 @@ import { ContainerError } from "../errors/container.error.js";
 import { BaseResolver } from "./base-resolver.js";
 
 export class KeyResolver extends BaseResolver implements ResolverContract {
-  protected _key: BindingKey;
+  protected _key: Resolvable;
 
-  public constructor(registry: RegistryContract, key: BindingKey) {
+  public constructor(registry: RegistryContract, key: Resolvable) {
     super(registry);
     this._key = key;
   }
 
   public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
-    const resolver = this._registry.getResolverByKey(this._key);
+    let resolveKey: BindingKey;
+    if (typeof this._key === "string") {
+      const pattern = /^([\p{Alpha}\p{N}]+)#([\p{Alpha}\p{N}]+)$/u;
+      const matched = this._key.match(pattern);
+      if (matched) resolveKey = [matched[1], matched[2]];
+    }
+    resolveKey = !Array.isArray(this._key)
+      ? ([this._key, BindingRoot] as BindingKey)
+      : (this._key as BindingKey);
+
+    const resolver = this._registry.getResolverByKey(resolveKey);
     if (typeof resolver === "undefined") {
       const toString = (bindingToken: BindingToken): string =>
         typeof bindingToken !== "string"
@@ -37,10 +49,10 @@ export class KeyResolver extends BaseResolver implements ResolverContract {
     if (resolver.scope === "transient") return resolver.resolve(scope, ...args);
 
     const cache = scope[resolver.scope];
-    return !cache.has(this._key)
+    return !cache.has(resolveKey)
       ? (cache
-          .set(this._key, resolver.resolve(scope, ...args))
-          .get(this._key) as Result)
-      : (cache.get(this._key) as Result);
+          .set(resolveKey, resolver.resolve(scope, ...args))
+          .get(resolveKey) as Result)
+      : (cache.get(resolveKey) as Result);
   }
 }
