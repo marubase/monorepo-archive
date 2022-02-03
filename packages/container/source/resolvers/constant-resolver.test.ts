f8595b4d75fad67725a36695a752821311fdaef3
Endy Jasmi <endyjasmi@gmail.com>
2022-02-03 22:32:01 +0800

    test(@marubase/container): update constant resolver test

--- packages/container/source/resolvers/constant-resolver.test.ts
+++ packages/container/source/resolvers/constant-resolver.test.ts
@@ -1,30 +1,24 @@
 import { expect } from "chai";
 import { instance, mock, reset } from "ts-mockito";
 import { RegistryContract } from "../contracts/registry.contract.js";
-import { ScopeContract } from "../contracts/scope.contract.js";
 import { ConstantResolver } from "./constant-resolver.js";
 
 describe("ConstantResolver", function () {
   let mockRegistry: RegistryContract;
-  let mockScope: ScopeContract;
   let registry: RegistryContract;
   let resolver: ConstantResolver;
-  let scope: ScopeContract;
   beforeEach(async function () {
     mockRegistry = mock();
-    mockScope = mock();
     registry = instance(mockRegistry);
-    scope = instance(mockScope);
     resolver = new ConstantResolver(registry, true);
   });
   afterEach(async function () {
     reset(mockRegistry);
-    reset(mockScope);
   });
 
   describe("#resolve(scope, ...args)", function () {
     it("should return resolved instance", async function () {
-      const instance = resolver.resolve(scope);
+      const instance = resolver.resolve();
       expect(instance).to.be.true;
     });
   });
