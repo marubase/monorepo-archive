6d5d7de10785cf86bae706d6a2bf6a1c30cde5c7
Endy Jasmi <endyjasmi@gmail.com>
2022-02-04 20:19:50 +0800

    test(@marubase/container): add resolvers index test

---
+++ packages/container/source/resolvers/index.test.ts
@@ -0,0 +1,8 @@
+import "./alias-resolver.test.js";
+import "./base-resolver.test.js";
+import "./class-resolver.test.js";
+import "./constant-resolver.test.js";
+import "./function-resolver.test.js";
+import "./key-resolver.test.js";
+import "./method-resolver.test.js";
+import "./tag-resolver.test.js";
