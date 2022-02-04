3f7036243bb6bcf945f3faf66850795cd349963e
Endy Jasmi <endyjasmi@gmail.com>
2022-02-04 20:21:00 +0800

    test(@marubase/container): update module index test

---
+++ packages/container/source/index.test.ts
@@ -0,0 +1,8 @@
+import "reflect-metadata";
+import "./cache.test.js";
+import "./container.test.js";
+import "./decorator.test.js";
+import "./registry-bind.test.js";
+import "./registry.test.js";
+import "./resolvers/index.test.js";
+import "./scope.test.js";
