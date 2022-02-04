b322f22d40611b0479053b1bf2de0864d081f054
Endy Jasmi <endyjasmi@gmail.com>
2022-02-04 20:18:41 +0800

    test(@marubase/collator): add codecs index test

---
+++ packages/collator/source/codecs/index.test.ts
@@ -0,0 +1,8 @@
+import "./boolean-codec.test.js";
+import "./buffer-codec.test.js";
+import "./complex-codec.test.js";
+import "./date-codec.test.js";
+import "./float-codec.test.js";
+import "./integer-codec.test.js";
+import "./string-codec.test.js";
+import "./version-codec.test.js";
