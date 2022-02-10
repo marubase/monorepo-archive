fa6c89f185cfc564c3a2e803a2502f2d9ae1824b
Endy Jasmi <endyjasmi@gmail.com>
2022-02-10 16:04:32 +0800

    feat(@marubase/service-manager): add request builder contract

---
+++ packages/service-manager/source/contracts/request-builder.contract.ts
@@ -0,0 +1,49 @@
+import { MessageBuilderContract } from "./message-builder.contract.js";
+
+export interface RequestBuilderContract extends MessageBuilderContract {
+  readonly method: HttpRequestMethod;
+
+  readonly url: URL;
+
+  build(): Request;
+
+  setMethod(method: HttpRequestMethod): this;
+
+  setUrl(url: URL | string, base?: string): this;
+}
+
+export type HttpRequestMethod =
+  | "ACL"
+  | "BIND"
+  | "CHECKOUT"
+  | "CONNECT"
+  | "COPY"
+  | "DELETE"
+  | "GET"
+  | "HEAD"
+  | "LINK"
+  | "LOCK"
+  | "M-SEARCH"
+  | "MERGE"
+  | "MKACTIVITY"
+  | "MKCALENDAR"
+  | "MKCOL"
+  | "MOVE"
+  | "NOTIFY"
+  | "OPTIONS"
+  | "PATCH"
+  | "POST"
+  | "PROPFIND"
+  | "PROPPATCH"
+  | "PURGE"
+  | "PUT"
+  | "REBIND"
+  | "REPORT"
+  | "SEARCH"
+  | "SOURCE"
+  | "SUBSCRIBE"
+  | "TRACE"
+  | "UNBIND"
+  | "UNLINK"
+  | "UNLOCK"
+  | "UNSUBSCRIBE";
