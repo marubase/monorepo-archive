b6e221a02e394ac8d35acf3201e93f2161afeafd
Endy Jasmi <endyjasmi@gmail.com>
2022-02-11 12:50:44 +0800

    refactor(@marubase/container): update function resolver implementation

--- packages/container/source/resolvers/function-resolver.ts
+++ packages/container/source/resolvers/function-resolver.ts
@@ -1,17 +1,20 @@
-import { RegistryContract } from "../contracts/registry.contract.js";
-import { ResolverContract } from "../contracts/resolver.contract.js";
-import { ScopeContract } from "../contracts/scope.contract.js";
+import { RegistryInterface } from "../contracts/registry.contract.js";
+import { ResolverInterface } from "../contracts/resolver.contract.js";
+import { ScopeInterface } from "../contracts/scope.contract.js";
 import { BaseResolver } from "./base-resolver.js";
 
-export class FunctionResolver extends BaseResolver implements ResolverContract {
+export class FunctionResolver
+  extends BaseResolver
+  implements ResolverInterface
+{
   protected _target: Function;
 
-  public constructor(registry: RegistryContract, target: Function) {
+  public constructor(registry: RegistryInterface, target: Function) {
     super(registry);
     this._target = target;
   }
 
-  public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
+  public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
     const target = this._target;
     const targetArgs = this.resolveDependencies(scope).concat(...args);
     return target(...targetArgs);
