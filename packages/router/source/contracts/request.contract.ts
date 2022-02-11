0c5ef13a91e7c877a98c4912cc15582f7b1ae304
Endy Jasmi <endyjasmi@gmail.com>
2022-02-11 15:11:11 +0800

    feat(@marubase/router): add request contract

---
+++ packages/router/source/contracts/request.contract.ts
@@ -0,0 +1,67 @@
+import { MessageInterface } from "./message.contract.js";
+
+export const RequestContract = Symbol("RequestContract");
+
+export interface RequestInterface extends MessageInterface {
+  readonly method: RequestMethod;
+
+  readonly url: URL;
+
+  setHash(hash: string): this;
+
+  setHost(host: string): this;
+
+  setMethod(method: RequestMethod): this;
+
+  setParam(key: string, value: Array<string> | string): this;
+
+  setPassword(password: string): this;
+
+  setPath(path: string): this;
+
+  setPort(port: number): this;
+
+  setScheme(scheme: string): this;
+
+  setURL(url: URL | string, base?: string): this;
+
+  setUsername(username: string): this;
+}
+
+export type RequestParams = Record<string, Array<string>>;
+
+export type RequestMethod =
+  | "ACL"
+  | "BIND"
+  | "CHECKOUT"
+  | "CONNECT"
+  | "COPY"
+  | "DELETE"
+  | "GET"
+  | "HEAD"
+  | "LINK"
+  | "LOCK"
+  | "M-SEARCH"
+  | "MERGE"
+  | "MKACTIVITY"
+  | "MKCALENDAR"
+  | "MKCOL"
+  | "MOVE"
+  | "NOTIFY"
+  | "OPTIONS"
+  | "PATCH"
+  | "POST"
+  | "PROPFIND"
+  | "PROPPATCH"
+  | "PURGE"
+  | "PUT"
+  | "REBIND"
+  | "REPORT"
+  | "SEARCH"
+  | "SOURCE"
+  | "SUBSCRIBE"
+  | "TRACE"
+  | "UNBIND"
+  | "UNLINK"
+  | "UNLOCK"
+  | "UNSUBSCRIBE";
