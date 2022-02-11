b1120679538c030693d5b176c1d2bc0c09e20f45
Endy Jasmi <endyjasmi@gmail.com>
2022-02-11 12:51:17 +0800

    refactor(@marubase/container): update tag resolver implementation

--- packages/container/source/resolvers/tag-resolver.ts
+++ packages/container/source/resolvers/tag-resolver.ts
@@ -1,21 +1,21 @@
 import {
   BindingTag,
-  RegistryContract,
+  RegistryInterface,
 } from "../contracts/registry.contract.js";
-import { ResolverContract } from "../contracts/resolver.contract.js";
-import { ScopeContract } from "../contracts/scope.contract.js";
+import { ResolverInterface } from "../contracts/resolver.contract.js";
+import { ScopeInterface } from "../contracts/scope.contract.js";
 import { BaseResolver } from "./base-resolver.js";
 
-export class TagResolver extends BaseResolver implements ResolverContract {
+export class TagResolver extends BaseResolver implements ResolverInterface {
   protected _tag: BindingTag;
 
-  public constructor(registry: RegistryContract, tag: BindingTag) {
+  public constructor(registry: RegistryInterface, tag: BindingTag) {
     super(registry);
     this._tag = tag;
   }
 
-  public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
-    const toInstance = (resolver: ResolverContract): unknown =>
+  public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
+    const toInstance = (resolver: ResolverInterface): unknown =>
       resolver.resolve(scope, ...args);
     const resolvers = this._registry.getResolverByTag(this._tag);
     return resolvers.map(toInstance) as unknown as Result;
