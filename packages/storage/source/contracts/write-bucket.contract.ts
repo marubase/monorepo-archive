72e066fdf10abb309108ced5662ed13408c77503
Endy Jasmi <endyjasmi@gmail.com>
2022-02-11 13:04:52 +0800

    feat(@marubase/storage): update write bucket contract

--- packages/storage/source/contracts/write-bucket.contract.ts
+++ packages/storage/source/contracts/write-bucket.contract.ts
@@ -1,6 +1,8 @@
 import { ReadBucketInterface } from "./read-bucket.contract.js";
 import { WriteTransactionInterface } from "./write-transaction.contract.js";
 
+export const WriteBucketContract = Symbol("WriteBucketContract");
+
 export interface WriteBucketInterface<Key, Value>
   extends ReadBucketInterface<Key, Value> {
   readonly transaction: WriteTransactionInterface;
