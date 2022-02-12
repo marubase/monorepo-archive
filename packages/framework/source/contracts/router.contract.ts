838cdd450fa82847aaa8d777e884993cc52c85f1
Endy Jasmi <endyjasmi@gmail.com>
2022-02-12 11:56:05 +0800

    feat(@marubase/framework): add router contract

---
+++ packages/framework/source/contracts/router.contract.ts
@@ -0,0 +1,19 @@
+import { ContextInterface } from "./context.contract.js";
+import { RequestInterface } from "./request.contract.js";
+import { ResponseInterface } from "./response.contract.js";
+
+export const RouterContract = Symbol("RouterContract");
+
+export interface RouterInterface {
+  dispatch(request: RequestInterface): Promise<ResponseInterface>;
+  dispatch(context: ContextInterface, next: NextFn): Promise<ResponseInterface>;
+
+  use(handler: HandleFn | RouterInterface): this;
+}
+
+export type HandleFn = (
+  context: ContextInterface,
+  next: NextFn,
+) => Promise<ResponseInterface>;
+
+export type NextFn = () => Promise<ResponseInterface>;
