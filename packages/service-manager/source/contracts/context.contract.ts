6a47256edb3512605989ec37cab3752f0af48742
Endy Jasmi <endyjasmi@gmail.com>
2022-02-12 15:09:30 +0800

    feat(@marubase/service-manager): add context contract

---
+++ packages/service-manager/source/contracts/context.contract.ts
@@ -0,0 +1,38 @@
+import { RequestMethod } from "./request.contract.js";
+import { ResponseInterface, StatusCode } from "./response.contract.js";
+
+export const ContextContract = Symbol("ContextContract");
+
+export interface ContextInterface {
+  readonly credential?: [string, string] | string;
+
+  readonly hash: string;
+
+  readonly hostname: string;
+
+  readonly method: RequestMethod;
+
+  readonly origin: string;
+
+  readonly params: Record<string, string>;
+
+  readonly path: string;
+
+  readonly port: number;
+
+  readonly queries: Record<string, string>;
+
+  readonly scheme: string;
+
+  readonly store: Record<string, unknown>;
+
+  clear(key: string): this;
+
+  get<Result>(key: string): Result | undefined;
+
+  has(key: string): boolean;
+
+  respondWith(statusCode: StatusCode, statusText?: string): ResponseInterface;
+
+  set(key: string, value: string): this;
+}
