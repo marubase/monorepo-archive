9299013a69825aa99e77f653e0a802055f4dd020
Endy Jasmi <endyjasmi@gmail.com>
2022-02-12 15:09:40 +0800

    feat(@marubase/service-manager): add context implementation

---
+++ packages/service-manager/source/context.ts
@@ -0,0 +1,94 @@
+import { ContextInterface } from "./contracts/context.contract.js";
+import { ManagerFactory } from "./contracts/manager.contract.js";
+import {
+  RequestInterface,
+  RequestMethod,
+} from "./contracts/request.contract.js";
+import {
+  ResponseInterface,
+  StatusCode,
+} from "./contracts/response.contract.js";
+
+export class Context implements ContextInterface {
+  protected _factory: ManagerFactory;
+
+  protected _params: Record<string, string> = {};
+
+  protected _request: RequestInterface;
+
+  protected _store: Record<string, unknown> = {};
+
+  public constructor(request: RequestInterface, factory: ManagerFactory) {
+    this._request = request;
+    this._factory = factory;
+  }
+
+  public get credential(): [string, string] | string | undefined {
+    return this._request.credential;
+  }
+
+  public get hash(): string {
+    return this._request.hash;
+  }
+
+  public get hostname(): string {
+    return this._request.hostname;
+  }
+
+  public get method(): RequestMethod {
+    return this._request.method;
+  }
+
+  public get origin(): string {
+    return this._request.origin;
+  }
+
+  public get params(): Record<string, string> {
+    return this._params;
+  }
+
+  public get path(): string {
+    return this._request.path;
+  }
+
+  public get port(): number {
+    return this._request.port;
+  }
+
+  public get queries(): Record<string, string> {
+    return this._request.queries;
+  }
+
+  public get scheme(): string {
+    return this._request.scheme;
+  }
+
+  public get store(): Record<string, unknown> {
+    return this._store;
+  }
+
+  public clear(key: string): this {
+    delete this._store[key];
+    return this;
+  }
+
+  public get<Result>(key: string): Result | undefined {
+    return this._store[key] as Result;
+  }
+
+  public has(key: string): boolean {
+    return key in this._store;
+  }
+
+  public respondWith(
+    statusCode: StatusCode,
+    statusText?: string,
+  ): ResponseInterface {
+    return this._factory.createResponse(statusCode, statusText);
+  }
+
+  public set(key: string, value: unknown): this {
+    this._store[key] = value;
+    return this;
+  }
+}
