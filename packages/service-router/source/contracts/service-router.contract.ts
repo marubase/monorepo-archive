805dc70d6a2ed6b9d925990ad0373d9eda64514e
Endy Jasmi <endyjasmi@gmail.com>
2022-02-13 19:33:17 +0800

    feat(@marubase/service-router): add service router contract

---
+++ packages/service-router/source/contracts/service-router.contract.ts
@@ -0,0 +1,49 @@
+import { ContainerInterface } from "@marubase/container";
+import { ServiceContextInterface } from "./service-context.contract.js";
+import {
+  ServiceRequestInterface,
+  ServiceRequestMethod,
+} from "./service-request.contract.js";
+import { ServiceResponseInterface } from "./service-response.contract.js";
+
+export const ServiceRouterContract = Symbol("ServiceRouterContract");
+
+export interface ServiceRouterInterface {
+  readonly container: ContainerInterface;
+
+  dispatch(request: ServiceRequestInterface): Promise<ServiceResponseInterface>;
+  dispatch(
+    context: ServiceContextInterface,
+    next: NextFn,
+  ): Promise<ServiceResponseInterface>;
+
+  handle(handler: HandleFn | ServiceRouterInterface): void;
+
+  method(method: ServiceRequestMethod[] | ServiceRequestMethod): MatchMethod;
+
+  path(path: string): MatchPath;
+
+  request(
+    method: ServiceRequestMethod,
+    path: string,
+  ): Promise<ServiceResponseInterface>;
+}
+
+export type ConfigureFn = (router: ServiceRouterInterface) => void;
+
+export type HandleFn = (
+  context: ServiceContextInterface,
+  next: NextFn,
+) => Promise<ServiceResponseInterface>;
+
+export type MatchMethod = {
+  handle(handler: HandleFn): void;
+
+  path(path: string): MatchPath;
+};
+
+export type MatchPath = {
+  handle(handler: HandleFn | ServiceRouterInterface): void;
+};
+
+export type NextFn = () => Promise<ServiceResponseInterface>;
