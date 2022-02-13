f05156ff9a5fa3c41600a34848b551f7a188431f
Endy Jasmi <endyjasmi@gmail.com>
2022-02-13 20:30:29 +0800

    refactor(@marubase/service-router): update service manager contract

--- packages/service-router/source/contracts/service-manager.contract.ts
+++ packages/service-router/source/contracts/service-manager.contract.ts
@@ -1,16 +1,31 @@
-import { Bindable } from "@marubase/container";
+import { ContainerInterface } from "@marubase/container";
+import {
+  ServiceRequestInterface,
+  ServiceRequestMethod,
+} from "./service-request.contract.js";
+import { ServiceResponseInterface } from "./service-response.contract.js";
 import { ServiceRouterInterface } from "./service-router.contract.js";
 
 export const ServiceManagerContract = Symbol("ServiceManagerContract");
 
 export interface ServiceManagerInterface {
+  readonly container: ContainerInterface;
+
   readonly hosts: Record<string, ServiceRouterInterface>;
 
-  readonly services: Map<Bindable, ServiceRouterInterface>;
+  readonly services: Record<string, ServiceRouterInterface>;
 
-  define(service: Bindable, defineFn: DefineFn): this;
+  define(service: string, defineFn: DefineFn): this;
 
-  host(origin: string, service: Bindable): this;
+  dispatch(request: ServiceRequestInterface): Promise<ServiceResponseInterface>;
+
+  host(origin: string, service: string): this;
+
+  request(
+    origin: string,
+    method: ServiceRequestMethod,
+    path: string,
+  ): ServiceRequestInterface;
 
   unhost(origin: string): this;
 }
