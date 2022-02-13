31230bb2d7db631c02895b609d36bd10c2b7864f
Endy Jasmi <endyjasmi@gmail.com>
2022-02-13 19:32:24 +0800

    feat(@marubase/service-router): add service context contract

---
+++ packages/service-router/source/contracts/service-context.contract.ts
@@ -0,0 +1,35 @@
+import { ContainerInterface } from "@marubase/container";
+import { StatusCode } from "../../../service-manager/source/contracts/response.contract.js";
+import { ServiceRequestMethod } from "./service-request.contract.js";
+import { ServiceResponseInterface } from "./service-response.contract.js";
+
+export const ServiceContextContract = Symbol("ContextContract");
+
+export interface ServiceContextInterface extends Map<unknown, unknown> {
+  readonly container: ContainerInterface;
+
+  readonly credential?: [string, string] | string;
+
+  readonly hash: string;
+
+  readonly hostname: string;
+
+  readonly method: ServiceRequestMethod;
+
+  readonly origin: string;
+
+  readonly params: Record<string, string>;
+
+  readonly path: string;
+
+  readonly port: number;
+
+  readonly queries: Record<string, string>;
+
+  readonly scheme: string;
+
+  replyWith(
+    statusCode: StatusCode,
+    statusText?: string,
+  ): ServiceResponseInterface;
+}
