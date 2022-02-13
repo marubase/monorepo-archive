b02e7b164e2d4ed24a5ad0c64fa0579017488fcc
Endy Jasmi <endyjasmi@gmail.com>
2022-02-13 23:11:00 +0800

    chore: remove @marubase/service-manager module

--- packages/service-manager/source/errors/service-manager.error.ts
+++
@@ -1,8 +0,0 @@
-import { StatusCode } from "../contracts/response.contract.js";
-
-export class ServiceManagerError extends Error {
-  public constructor(public statusCode: StatusCode, message?: string) {
-    super(message);
-    this.name = this.constructor.name;
-  }
-}
