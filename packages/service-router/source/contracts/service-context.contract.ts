d491ae4af70de14d5741ecccfa89e6d4ba99cb98
Endy Jasmi <endyjasmi@gmail.com>
2022-02-13 21:49:39 +0800

    refactor(@marubase/service-router): update service context contract

--- packages/service-router/source/contracts/service-context.contract.ts
+++ packages/service-router/source/contracts/service-context.contract.ts
@@ -1,16 +1,21 @@
-import { Callable, Resolvable } from "@marubase/container";
+import { ContainerInterface } from "@marubase/container";
 import { StatusCode } from "../../../service-manager/source/contracts/response.contract.js";
+import { ServiceManagerInterface } from "./service-manager.contract.js";
 import { ServiceRequestMethod } from "./service-request.contract.js";
 import { ServiceResponseInterface } from "./service-response.contract.js";
 
 export const ServiceContextContract = Symbol("ContextContract");
 
 export interface ServiceContextInterface extends Map<unknown, unknown> {
+  readonly container: ContainerInterface;
+
   readonly credential?: [string, string] | string;
 
   readonly hash: string;
 
   readonly hostname: string;
+
+  readonly manager: ServiceManagerInterface;
 
   readonly method: ServiceRequestMethod;
 
@@ -26,12 +31,8 @@ export interface ServiceContextInterface extends Map<unknown, unknown> {
 
   readonly scheme: string;
 
-  call<Result>(callable: Callable, ...args: unknown[]): Result;
-
   replyWith(
     statusCode: StatusCode,
     statusText?: string,
   ): ServiceResponseInterface;
-
-  resolve<Result>(resolvable: Resolvable, ...args: unknown[]): Result;
 }
