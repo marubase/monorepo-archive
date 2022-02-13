264477e51210ebe8aed72b17e2e6603cc781e5c8
Endy Jasmi <endyjasmi@gmail.com>
2022-02-13 14:57:33 +0800

    feat(@marubase/service-manager): add service manager error

--- packages/service-manager/source/errors/service-manager.error.ts
+++ packages/service-manager/source/errors/service-manager.error.ts
@@ -1,6 +1,6 @@
 import { StatusCode } from "../contracts/response.contract.js";
 
-export class ServiceError extends Error {
+export class ServiceManagerError extends Error {
   public constructor(public statusCode: StatusCode, message?: string) {
     super(message);
     this.name = this.constructor.name;
