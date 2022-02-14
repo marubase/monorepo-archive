a7ac714f33677517a6c422709b479ed8ce590dfc
Endy Jasmi <endyjasmi@gmail.com>
2022-02-14 11:30:44 +0800

    refactor(@marubase/service-manager): rename service router error

--- packages/service-manager/source/errors/service-manager.error.ts
+++ packages/service-manager/source/errors/service-manager.error.ts
@@ -1,6 +1,6 @@
 import { StatusCode } from "../contracts/service-response.contract.js";
 
-export class ServiceRouterError extends Error {
+export class ServiceManagerError extends Error {
   public constructor(public statusCode: StatusCode, message?: string) {
     super(message);
     this.name = this.constructor.name;
