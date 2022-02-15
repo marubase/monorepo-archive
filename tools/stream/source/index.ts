4040df1f7295e45f6e729a2499449ab8881ad514
Endy Jasmi <endyjasmi@gmail.com>
2022-02-15 18:21:35 +0800

    chore(@marubase-tools/stream): initial commit

---
+++ tools/stream/source/index.ts
@@ -0,0 +1,3 @@
+import { Duplex, Transform, Writable } from "readable-stream";
+import { Readable } from "./readable.js";
+export { Duplex, Readable, Transform, Writable };
---
+++ tools/stream/source/readable.ts
@@ -0,0 +1,91 @@
+import { Readable as BaseReadable, ReadableOptions } from "readable-stream";
+
+export class Readable extends BaseReadable {
+  public static from(
+    iterable: IterableInput,
+    options?: ReadableOptions,
+  ): Readable {
+    return new IterableReadable(iterable, options);
+  }
+}
+
+export class IterableReadable extends Readable {
+  protected _iterable: IterableInput;
+
+  public constructor(iterable: IterableInput, options: ReadableOptions = {}) {
+    super({ objectMode: isObjectMode(iterable), ...options });
+    this._iterable = iterable;
+  }
+
+  public _read(): void {
+    if (Array.isArray(this._iterable)) {
+      for (const item of this._iterable) this.push(item);
+      this.push(null);
+    } else if (
+      Buffer.isBuffer(this._iterable) ||
+      typeof this._iterable === "string"
+    ) {
+      this.push(this._iterable);
+      this.push(null);
+    } else if (this._iterable instanceof ArrayBuffer) {
+      this.push(Buffer.from(this._iterable));
+      this.push(null);
+    } else if (ArrayBuffer.isView(this._iterable)) {
+      const { buffer, byteLength, byteOffset } = this._iterable;
+      this.push(Buffer.from(buffer, byteOffset, byteLength));
+      this.push(null);
+    } else if (isIterable(this._iterable)) {
+      const result = this._iterable[Symbol.iterator]().next();
+      this.push(!result.done ? result.value : null);
+    } else if (isAsyncIterable(this._iterable)) {
+      this._iterable[Symbol.asyncIterator]()
+        .next()
+        .then((result) => {
+          this.push(!result.done ? result.value : null);
+        })
+        .catch((error) => {
+          this.destroy(error);
+        });
+    } else {
+      Promise.resolve(this._iterable.next())
+        .then((result) => {
+          this.push(!result.done ? result.value : null);
+        })
+        .catch((error) => {
+          this.destroy(error);
+        });
+    }
+  }
+}
+
+export function isAsyncIterable<T>(
+  iterable: AsyncIterable<T> | AsyncIterator<T> | Iterable<T> | Iterator<T>,
+): iterable is AsyncIterable<T> {
+  return Symbol.asyncIterator in iterable;
+}
+
+export function isIterable<T>(
+  iterable: AsyncIterable<T> | AsyncIterator<T> | Iterable<T> | Iterator<T>,
+): iterable is Iterable<T> {
+  return Symbol.asyncIterator in iterable;
+}
+
+export function isObjectMode(iterable: IterableInput): boolean {
+  return (
+    !Buffer.isBuffer(iterable) &&
+    !ArrayBuffer.isView(iterable) &&
+    !(iterable instanceof ArrayBuffer) &&
+    typeof iterable !== "string"
+  );
+}
+
+export type IterableInput =
+  | Array<unknown>
+  | ArrayBuffer
+  | AsyncIterable<unknown>
+  | AsyncIterator<unknown>
+  | Buffer
+  | Iterable<unknown>
+  | Iterator<unknown>
+  | NodeJS.TypedArray
+  | string;
