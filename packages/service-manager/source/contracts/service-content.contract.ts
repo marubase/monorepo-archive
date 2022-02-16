bc20760d04a028f761031455ac4f17dcc88f714e
Endy Jasmi <endyjasmi@gmail.com>
2022-02-16 12:54:32 +0800

    feat(@marubase/service-manager): add service content contract

---
+++ packages/service-manager/source/contracts/service-content.contract.ts
@@ -0,0 +1,35 @@
+import { Readable } from "@marubase-tools/stream";
+
+export const ServiceContentContract = Symbol("ServiceContentContract");
+
+export interface ServiceContentInterface {
+  readonly body: Readable;
+
+  readonly headers: Record<string, string>;
+
+  buffer(): Promise<Buffer>;
+
+  clearBody(): this;
+
+  clearHeader(key: string): this;
+
+  clearHeaders(): this;
+
+  json(): Promise<JsonData>;
+
+  setBody(body: ContentBody): this;
+
+  setHeader(key: string, value: string): this;
+
+  setHeaders(headers: Record<string, string>): this;
+}
+
+export type ContentBody = Buffer | JsonData | Readable;
+
+export type JsonData =
+  | { [property: string]: JsonData }
+  | JsonData[]
+  | boolean
+  | null
+  | number
+  | string;
