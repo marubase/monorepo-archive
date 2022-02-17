6cee92bb58ff2d456e03fc46d1ec7a60120b4565
Endy Jasmi <endyjasmi@gmail.com>
2022-02-17 11:44:58 +0800

    chore(@marubase-tools/http): initial commit

---
+++ tools/http/webpack.config.cjs
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = Object.assign(require("../../webpack.config.js"), {
+  devtool: "source-map",
+  entry: path.join(process.cwd(), "source", "index.ts"),
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    library: { name: ["MarubaseTools", "Http"], type: "assign-properties" },
+    path: path.join(process.cwd(), "build"),
+  },
+});
