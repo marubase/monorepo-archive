17e59b4ab09abe2307a83597040a3adb9fe4e52f
Endy Jasmi <endyjasmi@gmail.com>
2022-02-19 19:47:31 +0800

    feat(@marubase-tools/http-utils): add request contract

---
+++ tools/http-utils/source/contracts/request.contract.ts
@@ -0,0 +1,92 @@
+import { MessageInterface } from "./message.contract.js";
+
+export const RequestContract = Symbol("RequestContract");
+
+export interface RequestInterface extends MessageInterface {
+  readonly credential?: [string, string] | string;
+
+  readonly hash: string;
+
+  readonly hostname: string;
+
+  readonly method: RequestMethod;
+
+  readonly origin: string;
+
+  readonly path: string;
+
+  readonly port: number;
+
+  readonly protocol: string;
+
+  readonly queries: Record<string, string>;
+
+  readonly scheme: string;
+
+  clearCredential(): this;
+
+  clearHash(): this;
+
+  clearQueries(): this;
+
+  clearQuery(key: string): this;
+
+  setCredential(token: string): this;
+  setCredential(username: string, password?: string): this;
+
+  setHash(hash: string): this;
+
+  setHostname(hostname: string): this;
+
+  setMethod(method: RequestMethod): this;
+
+  setOrigin(origin: string): this;
+
+  setPath(path: string): this;
+
+  setPort(port: number): this;
+
+  setProtocol(protocol: string): this;
+
+  setQueries(queries: Record<string, string>): this;
+
+  setQuery(key: string, value: string): this;
+
+  setScheme(scheme: string): this;
+}
+
+export type RequestMethod =
+  | "ACL"
+  | "BIND"
+  | "CHECKOUT"
+  | "CONNECT"
+  | "COPY"
+  | "DELETE"
+  | "GET"
+  | "HEAD"
+  | "LINK"
+  | "LOCK"
+  | "M-SEARCH"
+  | "MERGE"
+  | "MKACTIVITY"
+  | "MKCALENDAR"
+  | "MKCOL"
+  | "MOVE"
+  | "NOTIFY"
+  | "OPTIONS"
+  | "PATCH"
+  | "POST"
+  | "PROPFIND"
+  | "PROPPATCH"
+  | "PURGE"
+  | "PUT"
+  | "REBIND"
+  | "REPORT"
+  | "SEARCH"
+  | "SOURCE"
+  | "SUBSCRIBE"
+  | "TRACE"
+  | "UNBIND"
+  | "UNLINK"
+  | "UNLOCK"
+  | "UNSUBSCRIBE";
